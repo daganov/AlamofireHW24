08b7587f751e418d603ac8da1283ef85185240fc
Denis Aganov <den.aganov@yandex.ru>
2022-02-02 12:29:11 +0300

    Initial Commit

---
+++ HW24 Alamofire/HW24_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW24_AlamofireApp.swift
+//  HW24 Alamofire
+//
+//  Created by Denis Aganov on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW24_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
